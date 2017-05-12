a4a7faf3352698d0641f24b71d3a82e508fcf924
Qian <qian.xie@csra.com>
2017-05-12 10:38:06 -0400

    Finished

--- refgendownload.sh
+++ refgendownload.sh
@@ -1,12 +1,10 @@
-(echo Step 1 Downloading iGenomes; date) | sed 'N;s/\n/ /'
+(echo Step 1 dbSNP_tbi; date) | sed 'N;s/\n/ /'
 echo
-(echo Step 2 Downloading iGenomes; date) | sed 'N;s/\n/ /'
+(echo Step 2 dbSNP_vcf; date) | sed 'N;s/\n/ /'
 echo
-(echo Step 3 Downloading iGenomes; date) | sed 'N;s/\n/ /'
+(echo Step 3 iGenomes; date) | sed 'N;s/\n/ /'
 echo
-(echo Step 4 Downloading iGenomes; date) | sed 'N;s/\n/ /'
+(echo Step 4 Extracting iGenomes; date) | sed 'N;s/\n/ /'
 echo
-(echo Step 5 Extracting iGenomes; date) | sed 'N;s/\n/ /'
-echo
-(echo Step 6 Downloading iGenomes; date) | sed 'N;s/\n/ /'
+(echo Step 5 Finished; date) | sed 'N;s/\n/ /'
 
