1bf0cdc71102d4ee147cb8f3b6eb7995acf0ec35
Qian <qian.xie@csra.com>
2017-05-12 10:57:48 -0400

    sleep1

--- refgendownload.sh
+++ refgendownload.sh
@@ -1,10 +1,10 @@
 (echo Step 1 dbSNP_tbi; date) | sed 'N;s/\n/ /'
-echo
+sleep 1
 (echo Step 2 dbSNP_vcf; date) | sed 'N;s/\n/ /'
-echo
+sleep 1
 (echo Step 3 iGenomes; date) | sed 'N;s/\n/ /'
-echo
+sleep 1
 (echo Step 4 Extracting iGenomes; date) | sed 'N;s/\n/ /'
-echo
+sleep 1
 (echo Step 5 Finished; date) | sed 'N;s/\n/ /'
 
